9a40a040f71f5d2d9fe345948bf659f1d6bf471c
txby <2394497386@qq.com>
2018-03-01 10:51:46 +0800

    Map设置

--- src/day6/SimpleHashMap.java
+++ src/day6/SimpleHashMap.java
@@ -1,18 +1,75 @@
 package day6;
 
-import java.util.AbstractMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class SimpleHashMap<K,V> extends AbstractMap<K,V> {
 
     static final int SIZE=997;
 
-
+    LinkedList<MapEntry<K,V>>[] buckets=new LinkedList[SIZE];
 
     @Override
-    public Set<Entry<K, V>> entrySet() {
+    public V put(K key,V value){
+        V oldValue=null;
+        int index=Math.abs(key.hashCode())%SIZE;
+        if (buckets[index]==null) {
+            buckets[index] = new LinkedList<>();
+
+        }
+         LinkedList<MapEntry<K,V>> bucket=buckets[index];
+
+            MapEntry<K,V> pair=new MapEntry<>(key,value);
+
+            boolean found=false;
+            ListIterator<MapEntry<K,V>> it=bucket.listIterator();
+
+            while (it.hasNext()){
+                MapEntry<K,V> iPair=it.next();
+
+                if (iPair.getKey().equals(key)){
+                    oldValue=iPair.getValue();
+                    //用新的替换旧的数据
+                    it.set(pair);
+                    found=true;
+                    break;
+                }
+            }
+
+            if (!found){
+                buckets[index].add(pair);
+            }
+        return oldValue;
+        }
+
+    @Override
+    public V get(Object key){
+        int index=Math.abs(key.hashCode())%SIZE;
+        if (buckets[index]==null){
+            return null;
+        }
+        for (MapEntry<K, V> iPair : buckets[index]) {
+            if (iPair.getKey().equals(key)){
+                return iPair.getValue();
+            }
+        }
         return null;
     }
+
+    @Override
+    public Set<Map.Entry<K, V>> entrySet() {
+         Set<Map.Entry<K,V>> set=new HashSet<>();
+         for (LinkedList<MapEntry<K,V>> bucket:buckets){
+             if (bucket==null){
+                 continue;
+             }
+             for (MapEntry<K,V> mPair:bucket){
+                 set.add(mPair);
+             }
+         }
+        return set;
+    }
+
+
+
 }
 
